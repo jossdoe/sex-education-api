029c682daa2a57f1ee385e3e3dcc55750cbea1e7
Joss Doebler <mail@joss-doebler.de>
2020-10-29 10:39:08 +0100

    Semantic descriptions on endpoint testing

--- src/__tests__/v1.test.js
+++ src/__tests__/v1.test.js
@@ -4,12 +4,12 @@ const request = require('supertest');
 
 const app = createServer();
 
-test('Root route has status 200 on GET', async () => {
+test('GET on "/v1/" returns status 200', async () => {
   const res = await request(app).get('/v1/');
   expect(res.status).toBe(200);
 });
 
-test('Root route returns JSON', async () => {
+test('GET on "/v1/" returns JSON with UTF-8', async () => {
   const res = await request(app).get('/v1/');
   expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
 });
