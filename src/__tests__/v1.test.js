5cae16691c9953c4a0d6950f6dca89e622d1e36d
Joss Doebler <mail@joss-doebler.de>
2020-10-29 10:43:13 +0100

    Added test for undefined routes

--- src/__tests__/v1.test.js
+++ src/__tests__/v1.test.js
@@ -13,3 +13,8 @@ test('GET on "/v1/" returns JSON with UTF-8', async () => {
   const res = await request(app).get('/v1/');
   expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
 });
+
+test('GET on undefined route returns status 404', async () => {
+  const res = await request(app).get('/undefined/');
+  expect(res.status).toBe(404);
+});
