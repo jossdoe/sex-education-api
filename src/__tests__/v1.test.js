b44eaa0ecb808eded7b4fd9c0e59bcbdcae5ec8c
Joss Doebler <mail@joss-doebler.de>
2020-10-29 10:37:05 +0100

    Remove listen() in endpoint testing

--- src/__tests__/v1.test.js
+++ src/__tests__/v1.test.js
@@ -5,11 +5,11 @@ const request = require('supertest');
 const app = createServer();
 
 test('Root route has status 200 on GET', async () => {
-  const res = await request(app.listen()).get('/v1/');
+  const res = await request(app).get('/v1/');
   expect(res.status).toBe(200);
 });
 
 test('Root route returns JSON', async () => {
-  const res = await request(app.listen()).get('/v1/');
+  const res = await request(app).get('/v1/');
   expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
 });
