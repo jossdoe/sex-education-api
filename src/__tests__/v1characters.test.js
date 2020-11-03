dd539c73faad59b05d86fdcce5d16d8fa6464623
Joss Doebler <mail@joss-doebler.de>
2020-11-03 16:07:49 +0100

    Additional tests with id property on characters route

--- src/__tests__/v1characters.test.js
+++ src/__tests__/v1characters.test.js
@@ -42,6 +42,7 @@ describe('GET all characters', () => {
   test(`GET on '${route}' returns Objects with correct properties`, async () => {
     const res = await request(app).get(route);
     expect(res.body[0]).toMatchObject({
+      id: expect.any(Number),
       name: expect.any(String),
       age: expect.any(String),
       gender: expect.any(String),
@@ -53,5 +54,24 @@ describe('GET all characters', () => {
   test(`GET on '${route}' returns Objects without _id property`, async () => {
     const res = await request(app).get(route);
     expect(res.body[0]._id).toBeUndefined();
+  });
+
+  test(`GET on '${route}' starts with id 1`, async () => {
+    const res = await request(app).get(route);
+    expect(res.body[0]).toMatchObject({
+      id: 1,
+    });
+  });
+
+  test(`GET on '${route}' ends with id 55`, async () => {
+    const res = await request(app).get(route);
+    expect(res.body[res.body.length - 1]).toMatchObject({
+      id: 55,
+    });
+  });
+
+  test(`GET on '${route}' returns exactly 55 episodes`, async () => {
+    const res = await request(app).get(route);
+    expect(res.body.length).toBe(55);
   });
 });
