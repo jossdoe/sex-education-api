933a35947888972661e14c1b334e5f52c3e5bc6e
Joss Doebler <mail@joss-doebler.de>
2020-11-03 16:09:34 +0100

    Add sorting by id to getAllCharacters controller

--- src/controllers/v1/getAllCharacters.js
+++ src/controllers/v1/getAllCharacters.js
@@ -3,8 +3,9 @@ const Character = require('../../models/Character');
 const getAllCharacters = async (req, res, next) => {
   try {
     const characters = await Character.find({}, { _id: 0 });
+    const sortedCharacters = characters.sort((a, b) => a.id - b.id);
 
-    res.status(200).json(characters);
+    res.status(200).json(sortedCharacters);
   } catch (e) {
     next(e);
   }
