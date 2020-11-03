9e918ddefdf290252eaef2f663443dc050dbf199
Joss Doebler <mail@joss-doebler.de>
2020-11-03 10:22:59 +0100

    Add try/catch and next(e) to characters & episodes routes

--- src/controllers/v1/getAllEpisodes.js
+++ src/controllers/v1/getAllEpisodes.js
@@ -1,10 +1,14 @@
 const Episode = require('../../models/Episode');
 
-const getAllEpisodes = async (req, res) => {
-  const episodes = await Episode.find({}, { _id: 0 });
-  const sortedEpisodes = [...episodes.sort((a, b) => a.id - b.id)];
+const getAllEpisodes = async (req, res, next) => {
+  try {
+    const episodes = await Episode.find({}, { _id: 0 });
+    const sortedEpisodes = [...episodes.sort((a, b) => a.id - b.id)];
 
-  res.status(200).json(sortedEpisodes);
+    res.status(200).json(sortedEpisodes);
+  } catch (e) {
+    next(e);
+  }
 };
 
 module.exports = getAllEpisodes;
