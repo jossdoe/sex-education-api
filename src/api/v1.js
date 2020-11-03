3b55de93d57b26121213c5de69c96080bf0e9e5a
Joss Doebler <mail@joss-doebler.de>
2020-11-03 10:10:40 +0100

    Add controller for getEpisodeById to route

--- src/api/v1.js
+++ src/api/v1.js
@@ -3,10 +3,12 @@ const api = express.Router();
 
 const helloWorld = require('../controllers/v1/helloWorld');
 const getAllEpisodes = require('../controllers/v1/getAllEpisodes');
+const getEpisodeById = require('../controllers/v1/getEpisodeById');
 const getAllCharacters = require('../controllers/v1/getAllCharacters');
 
 api.route('/').get(helloWorld);
 api.route('/episodes').get(getAllEpisodes);
+api.route('/episodes/:eid').get(getEpisodeById);
 api.route('/characters').get(getAllCharacters);
 
 module.exports = api;
