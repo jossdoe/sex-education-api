0326a74abb660d7a1f60b2df5748eb28e35e92b7
Joss Doebler <mail@joss-doebler.de>
2020-11-03 10:07:07 +0100

    Make error middleware return a JSON response

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,8 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res
+    .status(500)
+    .json({ error: true, code: 500, message: '500 - Server Error' });
 };
 
 module.exports = errorHandler;
