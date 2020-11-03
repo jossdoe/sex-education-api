1ba35b49cd0caf49e9f3aad15ac0af1698a5c04f
Joss Doebler <mail@joss-doebler.de>
2020-11-03 09:32:08 +0100

    Add description to episodes test

--- src/__tests__/v1episodes.test.js
+++ src/__tests__/v1episodes.test.js
@@ -18,47 +18,49 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-test(`GET on '${route}' returns status 200`, async () => {
-  const res = await request(app).get(route);
-  expect(res.status).toBe(200);
-});
-
-test(`GET on '${route}' returns JSON with UTF-8`, async () => {
-  const res = await request(app).get(route);
-  expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
-});
-
-test(`GET on '${route}' returns Array`, async () => {
-  const res = await request(app).get(route);
-  expect(R.is(Array, res.body)).toBeTruthy();
-});
-
-test(`GET on '${route}' returns Array of Objects`, async () => {
-  const res = await request(app).get(route);
-  expect(R.is(Object, res.body[0])).toBeTruthy();
-});
-
-test(`GET on '${route}' returns Objects with correct properties`, async () => {
-  const res = await request(app).get(route);
-  expect(res.body[0]).toMatchObject({
-    episode: expect.any(String),
-    name: expect.any(String),
-    synopsis: expect.any(String),
-    airdate: expect.any(String),
-    season: expect.any(String),
-    characters: expect.any(Array),
+describe('Get all episodes', () => {
+  test(`GET on '${route}' returns status 200`, async () => {
+    const res = await request(app).get(route);
+    expect(res.status).toBe(200);
   });
-});
 
-test(`GET on '${route}' returns Objects without _id property`, async () => {
-  const res = await request(app).get(route);
-  expect(res.body[0]._id).toBeUndefined();
-});
+  test(`GET on '${route}' returns JSON with UTF-8`, async () => {
+    const res = await request(app).get(route);
+    expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
+  });
 
-test(`GET on '${route}' starts with Episode 1 of Season 1`, async () => {
-  const res = await request(app).get(route);
-  expect(res.body[0]).toMatchObject({
-    episode: 'Episode 1',
-    season: '1',
+  test(`GET on '${route}' returns Array`, async () => {
+    const res = await request(app).get(route);
+    expect(R.is(Array, res.body)).toBeTruthy();
+  });
+
+  test(`GET on '${route}' returns Array of Objects`, async () => {
+    const res = await request(app).get(route);
+    expect(R.is(Object, res.body[0])).toBeTruthy();
+  });
+
+  test(`GET on '${route}' returns Objects with correct properties`, async () => {
+    const res = await request(app).get(route);
+    expect(res.body[0]).toMatchObject({
+      episode: expect.any(String),
+      name: expect.any(String),
+      synopsis: expect.any(String),
+      airdate: expect.any(String),
+      season: expect.any(String),
+      characters: expect.any(Array),
+    });
+  });
+
+  test(`GET on '${route}' returns Objects without _id property`, async () => {
+    const res = await request(app).get(route);
+    expect(res.body[0]._id).toBeUndefined();
+  });
+
+  test(`GET on '${route}' starts with Episode 1 of Season 1`, async () => {
+    const res = await request(app).get(route);
+    expect(res.body[0]).toMatchObject({
+      episode: 'Episode 1',
+      season: '1',
+    });
   });
 });
