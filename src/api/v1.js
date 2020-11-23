b53ff8ed6203654677276b56a55c3e3b1a02eb38
Joss Doebler <mail@joss-doebler.de>
2020-11-23 19:24:15 +0100

    Replace helloWorld controller in router with getCategories

--- src/api/v1.js
+++ src/api/v1.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const api = express.Router();
 
-const helloWorld = require('../controllers/v1/helloWorld');
+const getCategories = require('../controllers/v1/getCategories');
 const getAllEpisodes = require('../controllers/v1/getAllEpisodes');
 const getEpisodeById = require('../controllers/v1/getEpisodeById');
 const getAllCharacters = require('../controllers/v1/getAllCharacters');
 const getCharacterById = require('../controllers/v1/getCharacterById');
 
-api.route('/').get(helloWorld);
+api.route('/').get(getCategories);
 api.route('/episodes').get(getAllEpisodes);
 api.route('/episodes/:eid').get(getEpisodeById);
 api.route('/characters').get(getAllCharacters);
