be10d71c5f55c17c5599c5cd42a4a16983864f29
Joss Doebler <mail@joss-doebler.de>
2020-11-23 19:32:41 +0100

    Update and extend test suite for index route of v1

--- src/__tests__/v1.test.js
+++ src/__tests__/v1.test.js
@@ -1,30 +1,46 @@
 /* eslint-disable no-undef */
 const request = require('supertest');
+const R = require('ramda');
 
 const createServer = require('../createServer');
 
 const app = createServer();
 const route = '/v1/';
 
-test(`GET on ${route} returns status 200`, async () => {
-  const res = await request(app).get(route);
-  expect(res.status).toBe(200);
-});
+describe('Root endpoint & undefined routes', () => {
+  test(`GET on ${route} returns status 200`, async () => {
+    const res = await request(app).get(route);
+    expect(res.status).toBe(200);
+  });
 
-test(`GET on ${route} returns JSON with UTF-8`, async () => {
-  const res = await request(app).get(route);
-  expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
-});
+  test(`GET on ${route} returns JSON with UTF-8`, async () => {
+    const res = await request(app).get(route);
+    expect(res.headers['content-type']).toBe('application/json; charset=utf-8');
+  });
 
-test('GET on undefined route returns status 404', async () => {
-  const res = await request(app).get('/undefined/');
-  expect(res.status).toBe(404);
-});
+  test(`GET on '${route}' returns Object`, async () => {
+    const res = await request(app).get(route);
+    expect(R.is(Object, res.body)).toBeTruthy();
+  });
 
-test('GET on undefined route returns Error data in Object', async () => {
-  const res = await request(app).get('/undefined/');
-  expect(res.body).toMatchObject({
-    error: true,
-    code: 404,
+  test(`GET on '${route}' returns Objects with correct properties`, async () => {
+    const res = await request(app).get(route);
+    expect(res.body).toMatchObject({
+      characters: expect.any(String),
+      episodes: expect.any(String),
+    });
+  });
+
+  test('GET on undefined route returns status 404', async () => {
+    const res = await request(app).get('/undefined/');
+    expect(res.status).toBe(404);
+  });
+
+  test('GET on undefined route returns Error data in Object', async () => {
+    const res = await request(app).get('/undefined/');
+    expect(res.body).toMatchObject({
+      error: true,
+      code: 404,
+    });
   });
 });
